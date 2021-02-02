81900536147b2b5647269ce7a2bf4c99ed3bcca4
Aleksey Zabelin <zabelin.a.vla@sberbank.ru>
2021-02-03 00:48:52 +0700

    FIx profitability counting and balanced message

--- task05.py
+++ task05.py
@@ -17,11 +17,11 @@ def main():
 
     if total > 0:
         print(f'Your company is profitable, it earned: {total}')
-        print(f'Profitability: {profit / costs * 100}%')
+        print(f'Profitability: {total / costs * 100}%')
         workers_count = int(input('Enter number of workers in company:\n'))
         print(f'Profitability per worker: {total / workers_count}')
     elif total == 0:
-        print(f'Your company is balanced, it didn\'t earned anything')
+        print(f'Your company is balanced, it didn\'t earned or spent anything')
     else:
         print(f'Your company is NOT profitable, it spent: {total}')
 
