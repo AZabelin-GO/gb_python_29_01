91f59e382a7bf0b65a72b964647083cd91e9d729
Aleksey Zabelin <zabelin.a.vla@sberbank.ru>
2021-02-08 14:26:50 +0700

    LESSON-2: add task04

---
+++ lesson-2/task04.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Пользователь вводит строку из нескольких слов, разделённых пробелами. Вывести каждое слово с новой строки.
+Строки необходимо пронумеровать. Если в слово длинное, выводить только первые 10 букв в слове.
+"""
+
+
+def read_array():
+    str = input("Enter a string with words:\n")
+    return str.strip().split()
+
+
+def print_string_array(array):
+    for str in array:
+        print(f"{str:.10}")
+
+
+def main():
+    array = read_array()
+    print_string_array(array)
+
+
+if __name__ == '__main__':
+    main()
