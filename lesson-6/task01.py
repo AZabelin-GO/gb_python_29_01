ab145a003b18b2b62c6b910de993d3c9a9b43f1f
Aleksey Zabelin <zabelin.a.vla@sberbank.ru>
2021-02-23 19:44:48 +0700

    LESSON-6: add task01

--- lesson-6/task01.py
+++ lesson-6/task01.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from time import sleep
+
 """
 Создать класс TrafficLight (светофор) и определить у него один атрибут color (цвет) и метод running (запуск).
 Атрибут реализовать как приватный. В рамках метода реализовать переключение светофора
@@ -13,8 +15,40 @@
 """
 
 
+def print_progress(total_seconds):
+    for i in range(total_seconds):
+        print(".", end="")
+        sleep(1)
+    print()
+
+
+class TrafficLight:
+    states = [("Red", 7), ("Yellow", 2), ("Green", 15)]
+    __current_state = ""
+
+    def __init__(self):
+        self.__current_state = TrafficLight.states[0]
+
+    def running(self):
+        while True:
+            print(f"Current state: {self.__current_state[0]}")
+            self.__switch_state()
+
+    def __switch_state(self):
+        if self.__current_state == TrafficLight.states[0]:
+            print_progress(self.__current_state[1])
+            self.__current_state = TrafficLight.states[1]
+        elif self.__current_state == TrafficLight.states[1]:
+            print_progress(self.__current_state[1])
+            self.__current_state = TrafficLight.states[2]
+        elif self.__current_state == TrafficLight.states[2]:
+            print_progress(self.__current_state[1])
+            self.__current_state = TrafficLight.states[0]
+
+
 def main():
-    return
+    t_light = TrafficLight()
+    t_light.running()
 
 
 if __name__ == '__main__':
