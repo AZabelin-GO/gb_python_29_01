4da2683860caae9138bcbc16123ce9d6351a0a34
Aleksey Zabelin <zabelin.a.vla@sberbank.ru>
2021-02-23 20:31:44 +0700

    LESSON-6: add task03

--- lesson-6/task03.py
+++ lesson-6/task03.py
@@ -10,8 +10,47 @@ income (доход). Последний атрибут должен быть з�
 """
 
 
+def print_info(obj):
+    if isinstance(obj, Position):
+        msg = f"""
+            Full name: {obj.get_full_name()}
+            Position: {obj.position}
+            Income: {obj.get_total_income()}"""
+        print(msg)
+    elif isinstance(obj, Worker):
+        msg = f"""
+            Name: {obj.name}
+            Surname: {obj.surname}
+            Position: {obj.position}"""
+        print(msg)
+
+
+class Worker:
+    name = ""
+    surname = ""
+    position = ""
+    _income = {}
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+
 def main():
-    return
+    worker_one = Worker("John", "Smith", "Cleaner", 100, 50)
+    position_one = Position("Ann", "Smith", "Nurse", 75, 75)
+    print_info(worker_one)
+    print_info(position_one)
 
 
 if __name__ == '__main__':
