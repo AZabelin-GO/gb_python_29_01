85290ead1b37208310b8251899690d0e5f2bf7e0
Aleksey Zabelin <zabelin.a.vla@sberbank.ru>
2021-02-25 00:31:32 +0700

    LESSON-7: add task02

--- lesson-7/task02.py
+++ lesson-7/task02.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from abc import ABC, abstractmethod
+
 """
 Реализовать проект расчета суммарного расхода ткани на производство одежды. Основная сущность (класс) этого проекта —
 одежда, которая может иметь определенное название. К типам одежды в этом проекте относятся пальто и костюм.
@@ -12,8 +14,37 @@
 """
 
 
+class Clothes(ABC):
+    @property
+    @abstractmethod
+    def materials_consumption(self):
+        pass
+
+
+class Coat(Clothes):
+    def __init__(self, size):
+        self.size = size
+
+    @property
+    def materials_consumption(self):
+        return (self.size / 6.5) + 0.5
+
+
+class Costume(Clothes):
+    def __init__(self, height):
+        self.height = height
+
+    @property
+    def materials_consumption(self):
+        return 2 * self.height + 0.3
+
+
 def main():
-    return
+    my_coat = Coat(6.5)
+    my_costume = Costume(170)
+
+    print(my_coat.materials_consumption)
+    print(my_costume.materials_consumption)
 
 
 if __name__ == '__main__':
