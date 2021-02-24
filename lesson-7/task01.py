c4d7a33e37e0e59cab9720452f0c8d666fd8f1f4
Aleksey Zabelin <zabelin.a.vla@sberbank.ru>
2021-02-25 00:08:56 +0700

    LESSON-7: add task01

--- lesson-7/task01.py
+++ lesson-7/task01.py
@@ -13,8 +13,47 @@
 """
 
 
+class Matrix:
+    def __init__(self, data):
+        rows_length = [len(x) for x in data]
+
+        if rows_length.count(rows_length[0]) == len(rows_length):
+            self.data = data
+        else:
+            raise ValueError("Invalid data for matrix")
+
+    def __str__(self):
+        result = ""
+        for row in self.data:
+            result += f"{row}\n"
+        return result
+
+    def __eq__(self, other):
+        if not isinstance(other, Matrix):
+            return False
+        else:
+            return (len(self.data), len(self.data[0])) == (len(other.data), len(other.data[0]))
+
+    def __add__(self, other):
+        if not isinstance(other, Matrix):
+            raise TypeError
+        elif self.get_size() == other.get_size():
+            result = []
+            for i in range(len(self.data)):
+                result.append([sum(x) for x in zip(self.data[i], other.data[i])])
+
+            return Matrix(result)
+
+    def get_size(self):
+        return len(self.data), len(self.data[0])
+
+
 def main():
-    return
+    matrix = Matrix([[1, 2, 3], [2, 3, 1], [3, 1, 2]])
+    matrix2 = Matrix([[1, 2, 3], [2, 3, 1], [3, 1, 2]])
+    print(matrix)
+    print(matrix2)
+    print(matrix + matrix2)
 
 
 if __name__ == '__main__':
